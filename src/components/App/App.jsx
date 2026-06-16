9b4f8bf44d0430c7479716481b6bc0ddd9bbd6e8
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-06-16 21:05:49 +0300

    +

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,63 @@
-import UserControlledFormik from '../UserControlledFormik/UserControlledFormik'
+import { useEffect, useState } from 'react'
+import ArticleList from '../ArticleList/ArticleList'
+import { getArticles } from '../../api/articles-api'
+import SearchArticlesForm from '../SearchArticlesForm/SearchArticlesForm'
+import toast from 'react-hot-toast'
 
 const App = () => {
-  const login = (data) => {
-    console.log('data', data)
+  const [articles, setArticles] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [page, setPage] = useState(1)
+  const [searchValue, setSearchValue] = useState('')
+
+  useEffect(() => {
+    if (searchValue === '') return
+
+    const fetchArticles = async () => {
+      try {
+        setIsLoading(true)
+        const data = await getArticles(searchValue, page)
+        // setArticles([...articles, ...data.hits])
+        // setArticles((prevState) => {
+        //   return [...prevState, ...data.hits]
+        // })
+        setArticles((prevState) => [...prevState, ...data.hits])
+        toast.success('Articles done...')
+      } catch {
+        setError(true)
+        toast.error('Oops.. Something wrong.')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchArticles()
+  }, [searchValue, page])
+
+  const handleSearch = async (searchQuery) => {
+    setSearchValue(searchQuery)
+    setPage(1)
+    setArticles([])
   }
+
+  const handleLoadMore = async () => {
+    setPage(page + 1)
+  }
+
   return (
     <div>
-      <UserControlledFormik login={login} />
+      {error && <h2>Oops.. Something wrong. Pls reload page.</h2>}
+      <SearchArticlesForm onSearch={handleSearch} />
+      <hr />
+      {articles.length === 0 && isLoading && <h2>Loading..</h2>}
+      {articles.length > 0 && <ArticleList articles={articles} />}
+      {/* {isLoading && <h2>Loading..</h2>}
+      {articles.length > 0 && !isLoading && <button onClick={handleLoadMore}>Load more..</button>} */}
+      {articles.length > 0 && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          {isLoading ? 'Loading..' : 'Load more..'}
+        </button>
+      )}
     </div>
   )
 }
