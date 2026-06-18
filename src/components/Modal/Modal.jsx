05bc120f2e727b8c96707ef7f79e67aadb4491dc
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-06-18 21:08:38 +0300

    +

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,23 @@
+import { useContext } from 'react'
+import { langContext } from '../../LangContext'
+import { useLanguageContext } from '../../context/LanguageContextProvider'
+
 const Modal = ({ children }) => {
+  const langContext = useLanguageContext()
+  const some = useContext(langContext)
+  console.log('some in MODAL', some)
   return (
     <div>
       <h2>Modal Title</h2>
+      <select
+        name='lang'
+        value={langContext.language}
+        onChange={({ target: { value } }) => langContext.handleLang(value)}
+      >
+        <option value='ua'>UA</option>
+        <option value='en'>EN</option>
+        <option value='pl'>PL</option>
+      </select>
       {children}
     </div>
   )
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,16 +1,33 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Timer = () => {
+  const [counter, setCounter] = useState(0)
+
+  const intervalId = useRef()
+  // let intervalId
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    intervalId.current = setInterval(() => {
       console.log(Date.now())
     }, 1000)
     return () => {
-      clearInterval(intervalId)
+      clearInterval(intervalId.current)
     }
   }, [])
 
-  return <div>Timer</div>
+  // console.log('intervalId', intervalId.current)
+
+  const handleStop = () => {
+    console.log('intervalId', intervalId.current)
+    clearInterval(intervalId.current)
+  }
+
+  return (
+    <div>
+      <h2>Timer</h2>
+      <button onClick={handleStop}>Stop Timer</button>
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+    </div>
+  )
 }
 
 export default Timer
